3aaf28ca798b51aa4243bf30321eccfb08ef9ae8
Mirza Beig <37354140+MirzaBeig@users.noreply.github.com>
2018-03-14 16:31:26 -0400

    Add files via upload

---
+++ FireflyParticleAudio2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireflyParticleAudio2 : MonoBehaviour
+{
+    AudioSource audioSource;
+    public float lifetime = 1.0f;
+
+    public float pitchRandomizer = 1.0f;
+    public float volumeRandomizer = 0.25f;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+        audioSource.pitch += Random.Range(-pitchRandomizer / 2.0f, pitchRandomizer / 2.0f);
+        audioSource.volume += Random.Range(-volumeRandomizer / 2.0f, volumeRandomizer / 2.0f);
+
+        audioSource.Play();
+
+        Destroy(gameObject, lifetime);
+    }
+}
---
+++ FireflyParticleCollision2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireflyParticleCollision2 : MonoBehaviour
+{
+    ParticleSystem particleSystem;
+    List<ParticleCollisionEvent> particleCollisionEvents = new List<ParticleCollisionEvent>();
+
+    public GameObject instantiateOnParticleCollision;
+
+    void Start()
+    {
+        particleSystem = GetComponent<ParticleSystem>();
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        int numCollisionEvents = particleSystem.GetCollisionEvents(other, particleCollisionEvents);
+
+        for (int i = 0; i < numCollisionEvents; i++)
+        {
+            Instantiate(instantiateOnParticleCollision, particleCollisionEvents[i].intersection, Quaternion.identity);
+        }
+    }
+}
